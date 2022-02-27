6f9b89abae4283678f6e0dd14961f268b3aa25e0
Natarajan0512 <natarajan0512@gmail.com>
2022-02-27 17:51:50 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,7 @@ namespace API
 					options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 				});
             	services.AddControllers();
+				services.AddCors();
 	  	}
 	
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -48,6 +49,8 @@ namespace API
 			
 			app.UseHttpsRedirection();
 			app.UseRouting();
+
+			app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 			app.UseAuthorization();
 
